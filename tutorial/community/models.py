181599324727ce1ed8797c7d6388bf63868e9e03
shinevol53 <shinevol@gmail.com>
2020-07-29 23:15:45 +0900

    <note> tutorial1: bulletin board

---
+++ tutorial/community/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+# bulletin board class
+
+class Article(models.Model):
+    name = models.CharField(max_length=50)
+    title = models.CharField(max_length=50)
+    contents = models.TextField()
+    url = models.URLField()
+    email = models.EmailField()
+    cdate = models.DateTimeField(auto_now_add=True)
+
---
+++ tutorial/community/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render
+from community.forms import *
+# Create your views here.
+def write(request):
+    if request.method == 'POST':
+        form = Form(request.POST)
+        if form.is_valid():
+            form.save()
+    else:
+        form = Form()
+    form = Form()
+    return render(request, 'write.html', {'form':form})
+
+def list(request):
+    articlelist = Article.objects.all()
+    return render(request, 'list.html', {'articlelist':articlelist})
+
+def view(request, num="1"):
+    article = Article.objects.get(id=num)
+    return render(request, 'view.html', {'article':article})
